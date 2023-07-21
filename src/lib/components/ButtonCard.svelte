c00208fc4189fd7a7c393ee910851105421f6f73
JabSysEmb <abduljabbar.sha97@gmail.com>
2023-07-21 16:39:44 +0300

    button card

--- src/lib/components/ButtonCard.svelte
+++ src/lib/components/ButtonCard.svelte
@@ -20,21 +20,38 @@
   $: console.log(active);
 </script>
 
-<button on:click={toggle} class:active>
-  {!active ? "✅" : "❌"}
-</button>
+<div class:active>
+  <button on:click={toggle} class:active>
+    {!active ? "✅" : "❌"}
+  </button>
+</div>
 
 <style>
+  div {
+    border: 1px solid black;
+    border-radius: 15px;
+  }
+
+  div:hover {
+    background: hsla(0, 100%, 40%, 0.01);
+  }
+
+  div.active {
+    background: hsla(0, 100%, 10%, 0.1);
+  }
+
   button {
     width: 100px;
     height: 100px;
     border-radius: 10px;
     font-size: xx-large;
-    transition: all 0.5s ease-in;
+    margin: 10px;
+    transition: transform 0.5s ease-in, background 0.2s ease-in-out;
   }
 
   button:hover {
     cursor: pointer;
+    background: hsla(0, 100%, 1%, 0.2);
   }
 
   .active {
